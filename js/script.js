7b6342af7149c0847dffdfb40ed975757ffb2642
Aman Kumar Chaurasiya <amanc@techoon.in>
2026-01-07 10:24:18 +0530

    add dob validation

--- js/script.js
+++ js/script.js
@@ -1,50 +1,53 @@
-
 const students = [];
-
 
 const studentForm = document.getElementById("studentForm");
 const studentCountLabel = document.getElementById("studentCount");
 
-
 // Handle form submission
 studentForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
+  //store students input
+  const student = {
+    firstName: document.getElementById("firstName").value.trim(),
 
-    //store students input
-    const student = {
-        firstName: document.getElementById("firstName").value.trim(),
+    middleName: document.getElementById("middleName").value.trim(),
 
-        middleName: document.getElementById("middleName").value.trim(),
+    lastName: document.getElementById("lastName").value.trim(),
+    dateOfBirth: document.getElementById("dob").value,
 
-        lastName: document.getElementById("lastName").value.trim(),
-        dateOfBirth: document.getElementById("dob").value,
+    phoneNumber: document.getElementById("phone").value.trim(),
+    studentCourse: document.getElementById("course").value,
+  };
 
-        phoneNumber: document.getElementById("phone").value.trim(),
-        studentCourse: document.getElementById("course").value
-    };
+  // phone number validation
+  if (isNaN(student.phoneNumber) || student.phoneNumber.length !== 10) {
+    alert("Enter a valid 10-digit phone number");
+    return;
+  }
 
-     // phone number validation
-    if (isNaN(student.phoneNumber) || student.phoneNumber.length !== 10) {
-        alert("Enter a valid 10-digit phone number");
-        return;
-    }
-
-    if (!student.firstName || !student.lastName) {
+  if (!student.firstName || !student.lastName) {
     alert("First and Last name are required");
     return;
-}
+  }
 
+  const dob = new Date(student.dateOfBirth);
+  const today = new Date();
 
-    students.push(student);
+  if (dob > today) {
+    alert("Date of birth cannot be in the future");
+    return;
+  }
 
-    updateStudentCount();
-    studentForm.reset();
+  students.push(student);
 
-    console.log("Current Students List:", students);
+  updateStudentCount();
+  studentForm.reset();
+
+  console.log("Current Students List:", students);
 });
 
 //  Update registered student count
 function updateStudentCount() {
-    studentCountLabel.textContent = students.length;
+  studentCountLabel.textContent = students.length;
 }
